d0c7f239433e642d37824fb43a27b4595d8acfaf
Branislav Holländer <branislav.hollander@ge.com>
2018-03-03 17:47:51 +0100

    Testing k-means clustered datasets

--- test_mask_rcnn.py
+++ test_mask_rcnn.py
@@ -34,100 +34,68 @@ class InferenceConfig(CellConfig):
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
 
-inference_config = InferenceConfig()
+def test_mask_rcnn(test_ids, test_path, checkpoint_dir):
+    inference_config = InferenceConfig()
 
+    # Create the model in inference mode
+    print("Initializing model in inference mode ... ")
+    model = modellib.MaskRCNN(mode="inference", 
+                            config=inference_config,
+                            checkpoint_dir=checkpoint_dir,
+                            tensorboard_dir=TENSORBOARD_DIR)
 
-# Create the model in inference mode
-print("Initializing model in inference mode ... ")
-model = modellib.MaskRCNN(mode="inference", 
-                          config=inference_config,
-                          checkpoint_dir=CHECKPOINT_DIR,
-                          tensorboard_dir=TENSORBOARD_DIR)
+    # Get path to saved weights
+    # Either set a specific path or find last trained weights
+    # model_path = os.path.join(ROOT_DIR, ".h5 file name here")
+    model_path = model.find_last()
 
-# Get path to saved weights
-# Either set a specific path or find last trained weights
-# model_path = os.path.join(ROOT_DIR, ".h5 file name here")
-model_path = model.find_last()
+    # Load trained weights (fill in path to trained weights here)
+    print("Loading trained weights ... ")
+    assert model_path != "", "Provide path to trained weights"
+    print("Loading weights from ", model_path)
+    model.load_weights(model_path, by_name=True)
 
-# Load trained weights (fill in path to trained weights here)
-print("Loading trained weights ... ")
-assert model_path != "", "Provide path to trained weights"
-print("Loading weights from ", model_path)
-model.load_weights(model_path, by_name=True)
+    roi_class = []
 
+    print('Loading training images and masks ... ')
+    create_predicted_folders(test_ids)
 
+    dataset_test = CellsDataset()
+    dataset_test.load_cells(test_ids)
+    dataset_test.prepare()
 
-# Training dataset
-roi_class_train = []
+    # Evaluate dataset
+    print('Evaluating training dataset ... ')
+    results = []
+    for id in dataset_test.image_ids:
+        img = dataset_test.load_image(id)
+        results.append(model.detect([img], verbose=1))
 
-print('Loading training images and masks ... ')
-train_path='.\\data\\stage1_train_small\\'
-train_ids = next(os.walk(train_path))
-train_ids = [[train_ids[0] + d,d] for d in train_ids[1]]
-create_predicted_folders(train_ids)
+    print("Saving generated masks ...")
+    for i, res in tqdm(enumerate(results), total=len(results)):
+        path = os.path.join(dataset_test.image_info[i]["simple_path"], "masks_predicted")
+        mask = res[0]["masks"]
+        for j in range(mask.shape[2]):
+            io.imsave("{0}/mask_{1}.tif".format(path, j), mask[:,:,j] * 255)
 
-dataset_train = CellsDataset()
-dataset_train.load_cells(train_ids)
-dataset_train.prepare()
+        # also save other textual information retrieved by the CNN
+        class_ids = res[0]["class_ids"].tolist()
+        class_names = [x["name"] for x in dataset_test.class_info]
+        roi_class.append({"img": dataset_test.image_info[i]["simple_path"], "rois": [[i, tuple(r)] for (i,r) in enumerate(res[0]["rois"].tolist())], 
+                                "class_ids": class_ids, "class_names": class_names,
+                                "scores": res[0]["scores"].tolist()})
 
-# Evaluate training dataset
-print('Evaluating training dataset ... ')
-results = []
-for id in dataset_train.image_ids:
-    img = dataset_train.load_image(id)
-    results.append(model.detect([img], verbose=1))
+    with open(os.path.join(test_path, BBOX_CLASS_FNAME), 'w') as fp:
+        json.dump(roi_class, fp)
+    print("Done!")
 
-print("Saving generated masks ...")
-for i, res in tqdm(enumerate(results), total=len(results)):
-    path = os.path.join(dataset_train.image_info[i]["simple_path"], "masks_predicted")
-    mask = res[0]["masks"]
-    for j in range(mask.shape[2]):
-        io.imsave("{0}/mask_{1}.tif".format(path, j), mask[:,:,j] * 255)
+if __name__ == "__main__":
+    train_path='.\\data\\stage1_train_small\\'
+    train_ids = next(os.walk(train_path))
+    train_ids = [[train_ids[0] + d,d] for d in train_ids[1]]
+    test_mask_rcnn(train_ids, train_path, CHECKPOINT_DIR)
 
-    # also save other textual information retrieved by the CNN
-    class_ids = res[0]["class_ids"].tolist()
-    class_names = [x["name"] for x in dataset_train.class_info]
-    roi_class_train.append({"img": dataset_train.image_info[i]["simple_path"], "rois": [[i, tuple(r)] for (i,r) in enumerate(res[0]["rois"].tolist())], 
-                            "class_ids": class_ids, "class_names": class_names,
-                            "scores": res[0]["scores"].tolist()})
-
-with open(os.path.join(train_path, BBOX_CLASS_FNAME), 'w') as fp:
-    json.dump(roi_class_train, fp)
-print("Done!")
-
-# Testing dataset
-roi_class_test = []
-
-test_path='.\\data\\stage1_test\\'
-test_ids = next(os.walk(test_path))
-test_ids = [[test_ids[0] + d,d] for d in test_ids[1]]
-create_predicted_folders(test_ids)
-
-dataset_test = CellsDataset()
-dataset_test.load_cells(test_ids)
-dataset_test.prepare()
-
-# Evaluate testing dataset
-print('Evaluating testing dataset ... ')
-results = []
-for id in dataset_test.image_ids:
-    img = dataset_test.load_image(id)
-    results.append(model.detect([img], verbose=1))
-
-print("Saving generated masks ...")
-for i, res in tqdm(enumerate(results), total=len(results)):
-    path = os.path.join(dataset_test.image_info[i]["simple_path"], "masks_predicted")
-    mask = res[0]["masks"]
-    for j in range(mask.shape[2]):
-        io.imsave("{0}/mask_{1}.tif".format(path, j), mask[:,:,j] * 255)
-
-    # also save other textual information retrieved by the CNN
-    class_ids = res[0]["class_ids"].tolist()
-    class_names = [x["name"] for x in dataset_test.class_info]
-    roi_class_test.append({"img": dataset_test.image_info[i]["simple_path"], "rois": [[i, tuple(r)] for (i,r) in enumerate(res[0]["rois"].tolist())], 
-                            "class_ids": class_ids, "class_names": class_names,
-                            "scores": res[0]["scores"].tolist()})
-
-with open(os.path.join(test_path, BBOX_CLASS_FNAME), 'w') as fp:
-    json.dump(roi_class_test, fp)
-print("Done!")
+    test_path='.\\data\\stage1_test\\'
+    test_ids = next(os.walk(test_path))
+    test_ids = [[test_ids[0] + d,d] for d in test_ids[1]]
+    test_mask_rcnn(test_ids, test_path, CHECKPOINT_DIR)
--- test_separate.py
+++ test_separate.py
@@ -1,15 +1,34 @@
 import os
 from kmeans import test_kmeans 
-from test_mask_rcnn import 
+from test_mask_rcnn import test_mask_rcnn
 
-TRAIN_PATH = ".\\data\\stage1_train\\"
-VAL_PATH = ".\\data\\stage1_val\\"
+CHECKPOINT_DIR = os.path.join(ROOT_DIR, "checkpoints", "mask_rcnn")
 
-LEARNING_RATE = 0.001
- 
-val_ids = next(os.walk(VAL_PATH))
-val_ids = [[val_ids[0] + d,d] for d in val_ids[1]]
+TRAIN_PATH = ".\\data\\stage1_train_small\\"
+TEST_PATH = ".\\data\\stage1_test\\"
 
-print("Separating datasets using kmeans")
-clusters = train_kmeans(TRAIN_PATH, load_fitted=False)
-clusters = test
+# training dataset
+print("Separating training datasets using kmeans")
+clusters = test_kmeans(TRAIN_PATH)
+
+print("Testing training clusters")
+for i, cluster_ in enumerate(clusters):
+    print("Training cluster {0}".format(i))
+
+    # train mask RCNN for every separate cluster
+    checkpoint_dir = os.path.join(CHECKPOINT_DIR, "cluster_{0}".format(i))
+
+    test_mask_rcnn(cluster_, TRAIN_PATH, checkpoint_dir)
+
+# testing dataset
+print("Separating testing datasets using kmeans")
+clusters = test_kmeans(TEST_PATH)
+
+print("Testing testing clusters")
+for i, cluster_ in enumerate(clusters):
+    print("Testing cluster {0}".format(i))
+
+    # train mask RCNN for every separate cluster
+    checkpoint_dir = os.path.join(CHECKPOINT_DIR, "cluster_{0}".format(i))
+
+    test_mask_rcnn(cluster_, TEST_PATH, checkpoint_dir)
