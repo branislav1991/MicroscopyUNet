52ae715c49e72f5eebbf8f9539e9d25926e16f96
Branislav Hollander <branislav.hollander@gmail.com>
2018-03-13 07:37:41 +0100

    Remove shutdown from startup script

--- startup.sh
+++ startup.sh
@@ -5,30 +5,17 @@
 # Author: Branislav Hollaender
 # Date: 03/11/2018
 
-## wait for 30 seconds until everything loads
-## sleep 30
-
-## behave like superuser
-sudo apt-get update
-
-## change to workspace directory
-cd ~/workspace/MicroscopyUNet/
-
 ## delete previous instance_info.txt
 sudo rm instance_info.txt
 
 ## delete previous checkpoints
 sudo rm -r checkpoints/mask_rcnn
 
-## change pyenv to MLPlayground
-pyenv activate MLPlayground
-echo "Activated MLPlayground" >> instance_info.txt
-
 ## get current version of script on github
 git pull >> instance_info.txt
 
 ## run python script
-nohup sudo python train_mask_rcnn.py "./data/stage1_simple/" "./data/stage1_val/" >> instance_info.txt &
+nohup sudo ~/.pyenv/versions/MLPlayground/bin/python train_mask_rcnn.py "./data/stage1_simple/" "./data/stage1_val/" >> instance_info.txt &
 
 ## Shutdown instance. Note: this just shuts down the instance-not delete it.
-sudo shutdown -h now
+## sudo shutdown -h now
