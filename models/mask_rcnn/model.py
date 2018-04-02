Fix rotation augmentation

models/mask_rcnn/model.py
@@ -1292,11 +1292,11 @@ def load_image_gt(dataset, config, image_id, augment=False,
             image = exposure.adjust_gamma(image, gamma=1, gain=gain)
         if random.random() < probability:
             angle = random.random() * 90.0 - 45.0
-            center=tuple(np.array(image.shape[0:2])/2)
-            rot_mat = cv2.getRotationMatrix2D(center, angle, 0.7)
-            image = cv2.warpAffine(image, rot_mat, image.shape[0:2], flags=cv2.INTER_LINEAR)
+            center=tuple(np.array(image.shape[1::-1])//2)
+            rot_mat = cv2.getRotationMatrix2D(center, angle, 1)
+            image = cv2.warpAffine(image, rot_mat, img.shape[1::-1], flags=cv2.INTER_LINEAR)
             for i in range(mask.shape[2]):
-                mask[:,:,i] = cv2.warpAffine(mask[:,:,i], rot_mat, mask.shape[0:2], flags=cv2.INTER_NEAREST)
+                mask[:,:,i] = cv2.warpAffine(mask[:,:,i], rot_mat, mask.shape[1::-1], flags=cv2.INTER_NEAREST)
 
     # Bounding boxes. Note that some boxes might be all zeros
     # if the corresponding mask got cropped out.