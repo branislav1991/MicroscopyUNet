eb281747d7390c560c69b65fe1f571375f885b39
branislav <branislav.hollander@gmail.com>
2018-04-11 05:45:27 +0200

    Fewer epochs for head training

--- train_mask_rcnn.py
+++ train_mask_rcnn.py
@@ -103,7 +103,7 @@ if __name__ == "__main__":
     val_ids = [[val_ids[0] + d,d] for d in val_ids[1]]
 
     train_mask_rcnn(train_ids, val_ids, init_with="imagenet", checkpoint_dir=CHECKPOINT_DIR,
-         procedures=[{"layers": "heads", "learning_rate": LEARNING_RATE, "epochs": 20}])
+         procedures=[{"layers": "heads", "learning_rate": LEARNING_RATE, "epochs": 15}])
 
     train_mask_rcnn(train_ids, val_ids, init_with="last", checkpoint_dir=CHECKPOINT_DIR,
          procedures=[{"layers": "all", "learning_rate": LEARNING_RATE, "epochs": 60}])
